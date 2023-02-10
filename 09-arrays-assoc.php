345ca4605a74fc8a2eff967c0bc8a1b5268d9119
LuisMariscalDev <luisferdev16@gmail.com>
2023-02-10 13:47:10 -0600

    more arrays

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    "nombre" => "Luis",
+    "saldo" => "350",
+    "informacion" => [
+        "tipo" => "Premium",
+        "disponible" => "260"
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente["informacion"]);
+echo "</pre>";
 
+// echo $cliente["nombre"];
+// echo $cliente["informacion"]["disponible"];
+
+$cliente["Codigo"] = 1354684;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
