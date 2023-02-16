75da92481cc0e7ef26b7f8bb30c4e8498487fd69
LuisMariscalDev <luisferdev16@gmail.com>
2023-02-16 13:08:12 -0600

    functions

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,11 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1);
+include 'includes/header.php';
 
+function sumar( int $n1, int $n2) {
+    echo $n1 + $n2;
+}
 
-
-
+sumar(10, 20);
 
 include 'includes/footer.php';
