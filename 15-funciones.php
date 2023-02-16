7a5bfe5db684f82050bcc019a350b2bc9ec111da
LuisMariscalDev <luisferdev16@gmail.com>
2023-02-16 13:36:37 -0600

    named parameters

--- 15-funciones.php
+++ 15-funciones.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 include 'includes/header.php';
 
 function sumar( int $n1, int $n2) {
+    echo $n1;
     echo $n1 + $n2;
 }
 
-sumar(10, 20);
+sumar(n1: 10, n2: 20);
 
 include 'includes/footer.php';
