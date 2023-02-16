59414c4860123c1a0a531e4c173221405dbb28f4
LuisMariscalDev <luisferdev16@gmail.com>
2023-02-16 12:50:01 -0600

    foreach

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        "nombre" => "tablet",
+        "precio" => 8500,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Xbox",
+        "precio" => 7000,
+        "disponible" => true
+    ],
+    [
+        "nombre" => "Television",
+        "precio" => 5400,
+        "disponible" => false
+    ]
+];
 
+foreach( $productos as $producto ){ ?>
+    <li>
+        <p>Producto: <?php echo $producto["nombre"]; ?> </p>
+        <p>Precio: <?php echo "$" . $producto["precio"]; ?> </p>
+        <p> <?php echo ($producto["disponible"]) ? "Disponible" : "No disponible"; ?></p>
+    </li>
+    <?php
 
+}
 
 include 'includes/footer.php';
