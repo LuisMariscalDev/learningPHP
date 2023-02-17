7ca5c7d662d4625ec8b4095eb02b379a4ae6d0b8
LuisMariscalDev <luisferdev16@gmail.com>
2023-02-17 11:53:25 -0600

    require and include

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,12 @@
 <?php include 'includes/header.php';
 
+require "funciones.php";
 
+iniciarApp();
 
-
+//  Include aunque no encuentre el archivo sigue ejecuntándose 
+// require si no encuentra el archivo deja de ejecutar todo
+// require_once es igual que require solo que si el archivo ya fue incluido simplemente lo ignora
 
 
 include 'includes/footer.php';
