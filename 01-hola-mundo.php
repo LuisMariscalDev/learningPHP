98907f836be645e562ca54ec1dbeaaf470bd7f20
Luis Mariscal <luisferdev16@gmail.com>
2024-06-26 19:07:24 -0600

    first commit

---
+++ 01-hola-mundo.php
@@ -0,0 +1,12 @@
+<?php include 'includes/header.php';
+
+echo "Hola Mundo";
+echo ("hola mundo");
+
+print "Hola Mundo";
+print_r("Hola mundo");
+
+var_dump("Hola mundo");
+
+
+include 'includes/footer.php';
