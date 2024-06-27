c6af86f5ce956174b54b5ea2936260457adb267e
Luis Mariscal <luisferdev16@gmail.com>
2024-06-27 17:54:17 -0600

    arrays

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,38 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Computadora'];
+
+// Util para ver los contenidos de un array
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+// Acceder a un elemento del array
+echo $carrito[1];
+
+// Añade un elemento en el indice 3 del arreglo
+$carrito[3] = 'Nuevo producto...';
+
+// Añadir un elemento nuevo al final
+array_push($carrito, 'Audifonos');
+
+// Añadir un elemento al inicio
+array_unshift($carrito, 'Xbox');
 
 
+
+// Util para ver los contenidos de un array
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+
+$clientes = array('Cliente 1', 'Cliente 2', 'Cliente 3');
+// Util para ver los contenidos de un array
+echo "<pre>";
+var_dump($clientes);
+echo "</pre>";
+
+echo $clientes[1];
 
 include 'includes/footer.php';
--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,25 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Luis',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium',
+        'disponible' => '100'
+    ]
+];
 
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
+// echo $cliente['nombre'];
+// echo $cliente['informacion']['disponible'];
+
+$cliente['codigo'] = 168416351;
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
