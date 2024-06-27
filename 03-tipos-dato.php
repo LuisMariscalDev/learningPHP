8061bc9a8c7d7ffad33f2e35a89c81f1f65b65fc
Luis Mariscal <luisferdev16@gmail.com>
2024-06-26 22:43:24 -0600

    operadores

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,24 @@
 <?php include 'includes/header.php';
 
+// Boolean
+$login = true;
+var_dump($login);
 
+// Enteros
+$numero = 200;
+var_dump($numero);
+
+// Floats
+$float = 200.62;
+var_dump($float);
+
+// Strings
+$nombre = "Luis";
+var_dump($nombre);
+
+// Arreglos
+$array = [];
+var_dump($array);
 
 
 include 'includes/footer.php';
