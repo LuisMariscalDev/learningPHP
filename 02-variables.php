c792dac1dd7ca19518c063f64a3aa4c327c4c24e
Luis Mariscal <luisferdev16@gmail.com>
2024-06-26 22:04:24 -0600

    variables

--- 02-variables.php
+++ 02-variables.php
@@ -1,6 +1,20 @@
 <?php include 'includes/header.php';
 
+$nombre = 'Juan';
 
+$nombre = 'Luis';
+
+echo $nombre;
+var_dump($nombre);
+
+define('constante', "Este es el valor de la constante");
+echo constante;
+
+const constante2 = "Hola 2";
+echo constante2;
+
+$nombreCliente = "Antuan";
+$nombre_cliente = "Kross";
 
 
 include 'includes/footer.php';
