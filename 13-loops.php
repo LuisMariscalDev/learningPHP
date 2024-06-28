c608c2dd65321d0dc1d2755bd8cf63e40383fa60
Luis Mariscal <luisferdev16@gmail.com>
2024-06-27 23:07:30 -0600

    foreach

--- 13-loops.php
+++ 13-loops.php
@@ -1,5 +1,64 @@
 <?php include 'includes/header.php';
 
+// While
+
+// $i = 0; // Inicializador
+
+// while($i < 10) {
+
+//     echo $i . "<br>";
+
+//     $i++; // Incremento
+// }
+
+echo "<br>";
+
+// Do While
+$i = 100;
+
+do {
+    echo $i . "<br>";
+
+    $i++;
+} while($i < 10);
+
+echo "<br>";
+
+/*
+    3 Imprimir Fizz
+    5 Imprimir Buzz
+    3 y 5 imprimir Fizz Buzz
+*/
+
+// for($i=0; $i < 100; $i++) {
+//     if($i % 3 === 0 && $i % 5 ===0) {
+//         echo $i . " - Fizz Buzz <br>";
+//     } elseif($i % 3 === 0) {
+//         echo $i . "- Fizz <br>";
+//     } elseif($i % 5 === 0) {
+//         echo $i . " - Buzz <br>";
+//     } else {
+//         echo $i . "<br>";
+//     }
+// }
+
+
+// Foreach
+$clientes = array('Luis', 'Hanna', 'Fernanda');
+
+foreach($clientes as $cliente ){
+    echo  $cliente . "<br>";
+};
+
+$cliente = [
+    'nombre' => 'Antuan',
+    'tipo' => 'Premium',
+    'saldo' => 200
+];
+
+foreach($cliente as $key => $valor) {
+    echo $key . " - " . $valor . "<br>";
+}
 
 
 
--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'TV',
+        'precio' => 450,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Xbox',
+        'precio' => 3500,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Laptop',
+        'precio' => 300,
+        'disponible' => false
+    ]
+    ];
 
+foreach($productos as $producto) { ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre'];?></p>
+        <p>Precio: <?php echo $producto['precio'] ?></p>
+        <p>Disponible: <?php echo $producto['disponible'] ? 'Disponible' : 'No disponible'; ?></p>
+    </li>
 
+<?php
+}
 
 include 'includes/footer.php';
