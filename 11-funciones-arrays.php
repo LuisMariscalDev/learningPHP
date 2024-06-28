b9090da3e796d8b46b40a33ccd464fce64889e31
Luis Mariscal <luisferdev16@gmail.com>
2024-06-27 19:38:21 -0600

    funciones para arreglos

--- 11-funciones-arrays.php
+++ 11-funciones-arrays.php
@@ -1,7 +1,39 @@
 <?php include 'includes/header.php';
 
+// in_array - buscar elementos en un arreglo
+$carrito = ['tablet', 'computadora', 'television'];
+
+var_dump( in_array('tablet', $carrito));
+var_dump( in_array('Audifonos', $carrito));
+
+// Ordenar elementos de un arreglo
+$numeros = array(1,2,3,4,5,6,78);
+sort($numeros);
+rsort($numeros);
+
+echo "<pre>";
+var_dump($numeros);
+echo "</pre>";
+
+// Ordenar arreglo asociativo
+$cliente = [
+    'saldo' => 200,
+    'tipo' => 'Premium',
+    'nombre' => 'Luis'
+];
 
 
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
+asort($cliente); // Ordena por valores (orden alfabetico)
+arsort($cliente); // Ordena por valores (Z primero)
+ksort($cliente); // Ordena por llaves (orden alfabetico)
+krsort($cliente); // Ordena por llaves (orden alfabetico, de la Z a la A)
+
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 include 'includes/footer.php';
