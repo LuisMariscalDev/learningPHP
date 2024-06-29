4a0692ce86d973871c8f562f4f05b47267aeb513
Luis Mariscal <luisferdev16@gmail.com>
2024-06-28 21:34:11 -0600

    json en php

--- 18.json.php
+++ 18.json.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Televisión',
+        'precio' => 450,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Xbox',
+        'precio' => 3500,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Laptop',
+        'precio' => 300,
+        'disponible' => false
+    ]
+    ];
 
+echo "<pre>";
+var_dump($productos);
+
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE);
+
+$json_array = json_decode($json);
+
+var_dump($json);
+var_dump($json_array);
+echo "</pre>";
 
 
 
